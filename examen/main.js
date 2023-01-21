67a90cff4abd80d86395b81f4c77fdb4add1066a
kevinpaul99 <kevinpaul99999@gmail.com>
2023-01-21 14:05:37 -0500

    New changes 2.0 in the file main.js and file index.html

--- examen/main.js
+++ examen/main.js
@@ -15,3 +15,29 @@ function vh(){
     let sl=s.value;
     document.getElementById('rec').innerText = `${sl}.`;
 }
+function veri(){
+    var re=document.getElementById("num").value
+    if(re<=-1){
+        document.getElementById("w").innerHTML="El numero " + re + " es negativo";
+    }else{
+        document.getElementById("w").innerHTML="El numero "+ re + " es positivo";
+    }
+}
+function verifi(){
+    var n1 = document.getElementById("n1").value;
+    var n2 = document.getElementById("n2").value;
+    var n3 = document.getElementById("n3").value;
+    if(n1>n2 && n1>n3){
+        document.getElementById("e").innerHTML="El numero mayor es: " + n1;
+    }
+    if(n2>n1 && n2>n3){
+        document.getElementById("e").innerHTML="El numero mayor es: " + n2;
+    }
+    if(n3>n1 && n3>n2){
+        document.getElementById("e").innerHTML="El numero mayor es: " + n3;
+    }
+}
+function res(){
+    var n1 = document.getElementById("n1").value;
+    var n2 = document.getElementById("n2").value;
+}
