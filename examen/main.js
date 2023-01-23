5f01879d0d023b20c6546ba405631515c81e8373
kevinpaul99 <kevinpaul99999@gmail.com>
2023-01-23 17:39:14 -0500

    New changes 6.0 version alpha

--- examen/main.js
+++ examen/main.js
@@ -56,4 +56,29 @@ window.addEventListener("DOMContentLoaded",function(){
         document.getElementById('rsl').innerHTML = operacion; 
     }
     }); 
-  });
+});
+function edad(){
+    var ed=parseInt(document.getElementById("edad").value);
+    if(n1>=18){
+        document.getElementById("jh").innerHTML="Eres mayor de edad";
+    }else{
+        document.getElementById("jh").innerHTML="Eres menor de edad";
+    }
+}
+function Resul(){
+    var a =parseInt(document.getElementById("no1").value);
+    var b =parseInt(document.getElementById("no2").value);
+    var c =parseInt(document.getElementById("no3").value);
+    var d =parseInt(document.getElementById("no4").value);
+    var e =parseInt(document.getElementById("no5").value);
+    var reso=a+b+c+d+e;
+    var resf=reso/5;
+    document.getElementById("prome").innerHTML="Su promedio es: " +resf;
+    if(resf>=7){
+        document.getElementById("pasa").innerHTML="Usted pasa el año con "+resf;
+    }else if(resf<=6.99 && resf>=5){
+        document.getElementById("pasa").innerHTML="Ustede se queda en supletorios con "+resf;
+    }else if(resf<=4.99 && resf>=0){
+        document.getElementById("pasa").innerHTML="Usted se queda en remediales con la nota de "+resf;
+    }
+}
